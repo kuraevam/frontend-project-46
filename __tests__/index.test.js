00dd3c20d4c9fa3361f0d63248a1e2a7f28122e3
Kuraev Andrey <>
2023-06-17 14:15:29 +0300

    Add lint and test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,15 @@
-import genDiff from '../src/genDiff.js';
+/* global describe test expect */
+
 import path from 'path';
+import genDiff from '../src/genDiff.js';
 
-describe("Test 1", () => {
-    test('Сравнения плоских json-файлов', async () => {
-        const filepath1 = path.join(__dirname, '../__fixtures__/filepath1.json'); 
-        const filepath2 = path.join(__dirname, '../__fixtures__/filepath2.json'); 
+describe('Test 1', () => {
+  test('Сравнения плоских json-файлов', async () => {
+    const filepath1 = path.join(__dirname, '../__fixtures__/filepath1.json');
+    const filepath2 = path.join(__dirname, '../__fixtures__/filepath2.json');
 
-        const result = await genDiff(filepath1, filepath2);
-        expect(result).toBe(`{
+    const result = await genDiff(filepath1, filepath2);
+    expect(result).toBe(`{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -15,5 +17,5 @@ describe("Test 1", () => {
   + timeout: 20
   + verbose: true
 }`);
-    });
-})
+  });
+});
